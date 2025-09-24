5cc56a0fd24013e08581a26cc6f961026fabb743
OlegKrivenko <krivenko.olegus@gmail.com>
2025-09-24 22:44:17 +0200

    first commit

---
+++ src/redux/actions.js
@@ -0,0 +1,19 @@
+// import { createAction, nanoid } from '@reduxjs/toolkit';
+
+// export const addContact = createAction(
+//   'contacts/addContact',
+//   ({ name, number }) => {
+//     return { payload: { name, number, id: nanoid() } };
+//   }
+// );
+
+// export const deleteContact = createAction('contacts/deleteContact', id => {
+//   return { payload: { id } };
+// });
+
+// export const filterContacts = createAction(
+//   'contacts/filterContacts',
+//   filter => {
+//     return { payload: { filter } };
+//   }
+// );
---
+++ src/redux/selectors.js
@@ -0,0 +1,3 @@
+export const getContacts = state => state.contacts.contactsArray;
+
+export const getFilter = state => state.filter.query;
